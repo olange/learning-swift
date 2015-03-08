76ecf979c4fa49e00cb157ca060d0bc6d56cf479
Olivier Lange <olange@petit-atelier.ch>
2015-03-08 22:30:51 +0100

    Added «How Many Fingers» app skeleton (section 03)

---
+++ complete-apple-watch-course/section-03/How Many Fingers/How Many Fingers WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  How Many Fingers WatchKit Extension
+//
+//  Created by Olivier on 08.03.15.
+//  Copyright (c) 2015 Le Petit Atelier de Génie logiciel. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
